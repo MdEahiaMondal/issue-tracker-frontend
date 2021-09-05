8ba4aa1c8c7461f945ffe99b56789e0dbe737776
mdeahiamondal <mdeahiamondal@gmail.com>
2021-09-05 09:49:50 +0600

    login

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+      <h1>This is an Dashboard page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-red-400 m-auto h-60">
+    <div class="h-32 m-auto p-10">
+      <h2 class="font-bold text-3xl">Login</h2>
+      <div class="mb-3">
+        <label for="email">Email: </label>
+        <input v-model="email" type="text" class="p-2 rounded-full outline-none" id="email" placeholder="Email">
+      </div>
+      <div>
+        <label for="Password">Password: </label>
+        <input v-model="password" id="Password" class="p-2 rounded-full outline-none" type="text"
+               placeholder="Password">
+      </div>
+
+      <button @click="login" class="p-2 bg-white text-red-400 mt-3 rounded-full px-7">Login</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      email: 'mdeahiyakhan@gmail.com',
+      password: '12345678'
+    }
+  },
+  methods: {
+    login() {
+      axios.post(' http://127.0.0.1:8000/api/login/', {
+        email: this.email,
+        password: this.password,
+      })
+      .then(res => {
+        this.$router.replace('dashboard')
+        localStorage.setItem('token', res.data.access_token)
+      })
+    }
   }
 }
 </script>
